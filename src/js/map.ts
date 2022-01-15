1fab531a892c87f502666ca33761b2f37bd47375
Taylor Smith <tsmith@tsmithcreative.com>
2022-01-15 12:08:42 -0600

    Restructure map.js by component and wrap GeolocateControl in a support check

--- src/js/map.ts
+++ src/js/map.ts
@@ -12,9 +12,17 @@ interface SimpleLngLat {
   lat: number,
 };
 
+// @TODO: This gets passed client-side, so it's not a secret, but it'd be nifty
+// if it weren't in the codebase. Env var to Parcel at build-time?
 const accessToken = 'pk.eyJ1IjoidHNtaXRoNTEyIiwiYSI6ImNreGp2NDQ2ODBvMHkybnBuYmk5bXJ5a3QifQ.5W22pLLWNQVOVj6pLgaE6Q';
 
-const getTxDistrict = (position: SimpleLngLat) => {
+/**
+ * Look up a given position with Mapbox's Tilequery API to see what TX district
+ * the position falls into and raise a popup on the map with the info.
+ *
+ * @param position (SimpleLngLat) position to query for
+ */
+const getTxDistrict = (position: SimpleLngLat): void => {
   const data = fetch(`https://api.mapbox.com/v4/tsmith512.ccvoi5im/tilequery/${position.lng},${position.lat}.json?access_token=${accessToken}`)
     .then(res => res.json())
     .then(payload => {
@@ -30,6 +38,8 @@ const getTxDistrict = (position: SimpleLngLat) => {
       }
     });
 };
+
+// Map Interface Pieces
 
 const map = new Map({
   accessToken,
@@ -47,6 +57,18 @@ const popup = new Popup({
   className: 'district-popup',
 });
 
+//
+// Simple Click/Tap Action
+//
+
+map.on('click', (e) => {
+  getTxDistrict(e.lngLat);
+});
+
+//
+// Geocoder: Type an address to search
+//
+
 const geocoder = new MapboxGeocoder({
   accessToken,
   marker: false,
@@ -55,24 +77,6 @@ const geocoder = new MapboxGeocoder({
 
 map.addControl(geocoder);
 
-const locator = new GeolocateControl({
-  positionOptions: {
-    enableHighAccuracy: true,
-  },
-  fitBoundsOptions: {
-    maxZoom: 12,
-  },
-  trackUserLocation: false,
-  showUserLocation: false,
-  showAccuracyCircle: false,
-});
-
-map.addControl(locator);
-
-map.on('click', (e) => {
-  getTxDistrict(e.lngLat);
-});
-
 geocoder.on('result', (results) => {
   const point = results.result?.center || false;
   if (point) {
@@ -80,14 +84,65 @@ geocoder.on('result', (results) => {
   }
 });
 
-locator.on('geolocate', (data: any) => {
-  if (data && data.hasOwnProperty('coords')) {
-    const latLng = {
-      lat: data.coords.latitude,
-      lng: data.coords.longitude,
-    };
+//
+// Geolocator: Use my device's position to search.
+//
+// Note: Candidate's site is on Wix, which won't let me allow geolocation within
+// the iframe, so check if the map page was loaded with a query arg before even
+// displaying this.
+//
+// Lifted from mapbox-gl-js/geolocate_control.js:checkGeolocationSupport()
+// because it isn't exported. Check for location support/allowed before showing
+// the button. Why? I don't want to show a disabled button.
+//
 
-    geocoder.clear(data);
-    getTxDistrict(latLng);
+const ifGeoSupported = (callback: Function): void => {
+  let supportsGeolocation = false;
+
+  if (window.navigator.permissions !== undefined) {
+    // navigator.permissions has incomplete browser support
+    // http://caniuse.com/#feat=permissions-api
+    // Test for the case where a browser disables Geolocation because of an
+    // insecure origin
+    window.navigator.permissions.query({name: 'geolocation'}).then((p) => {
+      supportsGeolocation = p.state !== 'denied';
+      callback(supportsGeolocation);
+    });
+  } else {
+    supportsGeolocation = !!window.navigator.geolocation;
+    callback(supportsGeolocation);
   }
-});
+};
+
+const setupGeolocator = (supported: boolean): void => {
+  if (!supported) {
+    return;
+  }
+
+  const locator = new GeolocateControl({
+    positionOptions: {
+      enableHighAccuracy: true,
+    },
+    fitBoundsOptions: {
+      maxZoom: 12,
+    },
+    trackUserLocation: false,
+    showUserLocation: false,
+    showAccuracyCircle: false,
+  });
+
+  map.addControl(locator);
+
+  locator.on('geolocate', (data: any) => {
+    if (data && data.hasOwnProperty('coords')) {
+      const latLng = {
+        lat: data.coords.latitude,
+        lng: data.coords.longitude,
+      };
+
+      getTxDistrict(latLng);
+    }
+  });
+};
+
+ifGeoSupported(setupGeolocator);
